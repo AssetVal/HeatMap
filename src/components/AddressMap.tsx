2e1f4e5113eafbd67442edd55d9c7243ace2da69
Abourass <Abourassa@AssetVal.com>
2024-12-17 09:43:32 -0700

    Fix unit toggle positioning in AddressMap component for improved layout

--- src/components/AddressMap.tsx
+++ src/components/AddressMap.tsx
@@ -275,7 +275,7 @@ export function AddressMap(props: Props) {
       />
 
       {/* Add unit toggle */}
-      <div class="absolute bottom-[22.5DVH] right-0 px-4 rounded">
+      <div class="absolute bottom-[23dvh] right-0 px-4 rounded">
         <button
           onClick={() => {
             actions.toggleUnit();
